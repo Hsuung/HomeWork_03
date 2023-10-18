840b78546b6974acb05f015b3cf7fc2f84bb3ab9
Hsu Tung <e403857054@gmail.com>
2023-10-18 18:04:45 +0800

    add hw2

--- src/hw2/hw2.java
+++ src/hw2/hw2.java
@@ -10,6 +10,40 @@ public class hw2 {
 				i += count;}
 		}
 		System.out.println(i);
+		
+		
+		System.out.println("========================================");
+		
+		int a = 1;
+		
+		
+		for (int b = 1; b <= 10; b++) {
+			a *= b;
+			
+		}
+		System.out.println(a);
+		
+		
+		System.out.println("========================================");
+		
+		int x = 1;
+		int y = 1;
+		
+		while (y <= 10) {
+			x *= y;
+			y++;
+		}
+		System.out.println(x);
+		
+		
+		System.out.println("========================================");
+		
+		int n = 1;
+		
+		for (int m = 1; m <= 10; m++) {
+			n = m * m;
+			System.out.println(n);
+		}
 	}
 
 }
